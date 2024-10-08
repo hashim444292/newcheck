4a52c65192991d52e862588c5383a1b190734f8a
hashim444292 <hashim.hameedgaziani@gmail.com>
2024-10-08 05:54:56 +0500

    nested if

--- newdart.dart
+++ newdart.dart
@@ -1,3 +1,16 @@
 void main() {
-  print("hello world");
+  String user = "saeer";
+  String pass = "sameer23";
+
+  if (user == "sameer" || pass == "sameer123") {
+    if (user == "sameer" && pass == "sameer123") {
+      print("successfuly Login");
+    } else if (user == "sameer") {
+      print("your user name is correct but password is wrong");
+    } else {
+      print(" user name is wrong  but your  password is correct");
+    }
+  } else {
+    print(" Your user name or password both are wrong");
+  }
 }
